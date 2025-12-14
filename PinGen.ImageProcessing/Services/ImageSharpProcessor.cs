5fd152ee344a005ab9ca27f6f602303801ee77fb
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-12-14 11:06:14 -0500

    Tried messing with onnx ML package but it was butchering the photos. Doesnt seem like there is free reliable background removal that can be used for a package or api, the api's are paywalled with the free tiers not being worth it. Going to move on and come back to this feature later so for now just recommending users to run item images through Adobe Express Background Remover online and then use photos with my program.

--- PinGen.ImageProcessing/Services/ImageSharpProcessor.cs
+++ PinGen.ImageProcessing/Services/ImageSharpProcessor.cs
@@ -10,58 +10,6 @@ namespace PinGen.ImageProcessing.Services
 {
     public class ImageSharpProcessor : IImageProcessor
     {
-        //public BitmapSource RemoveWhiteBackground(string imagePath, byte tolerance = 15)
-        //{
-        //    using Image<Rgba32> image = Image.Load<Rgba32>(imagePath);
-
-        //    image.ProcessPixelRows(accessor =>
-        //    {
-        //        for (int y = 0; y < accessor.Height; y++)
-        //        {
-        //            var row = accessor.GetRowSpan(y);
-        //            for (int x = 0; x < row.Length; x++)
-        //            {
-        //                ref Rgba32 pixel = ref row[x];
-
-        //                // If pixel is near white, make it transparent
-        //                if (pixel.R >= (255 - tolerance) &&
-        //                    pixel.G >= (255 - tolerance) &&
-        //                    pixel.B >= (255 - tolerance))
-        //                {
-        //                    pixel.A = 0;
-        //                }
-        //            }
-        //        }
-        //    });
-
-        //    using var ms = new MemoryStream();
-        //    image.SaveAsPng(ms);
-        //    ms.Position = 0;
-
-        //    var bmp = new BitmapImage();
-        //    bmp.BeginInit();
-        //    bmp.StreamSource = ms;
-        //    bmp.CacheOption = BitmapCacheOption.OnLoad;
-        //    bmp.EndInit();
-        //    bmp.Freeze();
-
-        //    return bmp;
-        //}
-
-        //public BitmapSource LoadAndPrepare(string path, int targetWidth, int targetHeight)
-        //{
-        //    using var image = Image.Load<Rgba32>(path);
-
-        //    image.Mutate(x => x.Resize(new ResizeOptions
-        //    {
-        //        Size = new Size(targetWidth, targetHeight),
-        //        Mode = ResizeMode.Max,
-        //        Sampler = KnownResamplers.Lanczos3 // IMPORTANT
-        //    }));
-
-        //    return ToBitmapSource(image);
-        //}
-
         private static BitmapSource ToBitmapSource(Image<Rgba32> image)
         {
             var pixels = new byte[image.Width * image.Height * 4];
