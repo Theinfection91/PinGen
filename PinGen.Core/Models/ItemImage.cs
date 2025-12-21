76d799aa65d67eab9626cbe3bce20b5002c96415
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-12-20 22:04:46 -0500

    Now have listbox that displays all curent file's full source path for one's selected. Still can't remove from list yet though.

--- PinGen.Core/Models/ItemImage.cs
+++ PinGen.Core/Models/ItemImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace PinGen.Core.Models
@@ -7,6 +8,7 @@ namespace PinGen.Core.Models
     public class ItemImage
     {
         public string SourcePath { get; set; } = string.Empty;
+        public string FileName => Path.GetFileName(SourcePath);
         public string? AltText { get; set; }
     }
 }
